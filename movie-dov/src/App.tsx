baa1d88591b4994886fdaf4e61b90be533b06722
dyschaf <schaffeldovid@gmail.com>
2025-06-07 21:16:19 +0300

    vercel

--- movie-dov/src/App.tsx
+++ movie-dov/src/App.tsx
@@ -8,9 +8,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
+
 function App() {
   
-  
+
   return (
     <>
       <Analytics/>
