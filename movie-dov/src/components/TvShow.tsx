d3aff670e43e22d95009bd0d5ff5788210accad8
dyschaf <schaffeldovid@gmail.com>
2025-07-22 18:22:32 +0300

    sources

--- movie-dov/src/components/TvShow.tsx
+++ movie-dov/src/components/TvShow.tsx
@@ -375,6 +375,7 @@ const TvShow: React.FC<{ id: number; historySelect: any; setSearchType: React.Di
     `https://vidjoy.pro/embed/tv/${id}/${selectedSeason}/${selectedEpisode.episode_number}`
     // ""
 
+
     
     // `https://9animetv.to`
   ] : [];
